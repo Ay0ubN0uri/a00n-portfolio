0417bd510006d05a385c3af73ba2e3781188821c
Ay0ub N0uri <ayoub.nouri105@gmail.com>
2023-05-15 20:44:09 +0100

    initial commit

---
+++ src/router/AllRoutes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import HomeDarkParticles from "../views/HomeDarkParticles";
+import NotFound from "../views/NotFound";
+import { Routes, Route } from "react-router-dom";
+import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
+
+const AllRoutes = () => {
+  return (
+    <>
+      <ScrollTopBehaviour />
+      <Routes>
+        <Route path="/" element={<HomeDarkParticles />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AllRoutes;
